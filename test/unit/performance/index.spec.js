c50f8b44aea7a676ae8d9dbe7d2aa6b8fd3059a4
Richard Lindsey <richard.lindsey@bazaarvoice.com>
2017-12-06 17:20:46 -0600

    SWAT-2616 -- Adding a couple more polyfilled functions to the performance polyfill, for getEntries and getEntriesByName, moving in-memory storage for marks and timeline out into its own module for sharing between marks and getEntries, and adding tests for new functions

--- test/unit/performance/index.spec.js
+++ test/unit/performance/index.spec.js
@@ -8,6 +8,14 @@ var perfModule = require('../../../lib/performance');
 
 describe('lib/performance', function () {
 
+  it('exposes a getEntries function', function () {
+    expect(perfModule.getEntries).to.be.a('function');
+  });
+
+  it('exposes a getEntriesByName function', function () {
+    expect(perfModule.getEntriesByName).to.be.a('function');
+  });
+
   it('exposes a now function', function () {
     expect(perfModule.now).to.be.a('function');
   });
