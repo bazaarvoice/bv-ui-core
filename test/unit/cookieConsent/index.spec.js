e73954bcc7c6560b220f6ae8797db95a548687b6
soumen-saniel <soumen.saniel@bazaarvoice.com>
2021-07-05 16:50:41 +0530

    Add: test cases

---
+++ test/unit/cookieConsent/index.spec.js
@@ -0,0 +1,90 @@
+/**
+ * @fileOverview
+ * Unit tests for the cookieConsent module.
+ */
+var cookieConsent = require('../../../lib/cookieConsent');
+
+describe('lib/cookieConsent', function () {
+  it('cookieConsent.initConsent', function () {
+    function test1 () {
+      cookieConsent.initConsent(true);
+    }
+
+    expect(test1).to.throw(TypeError, 'cookieConsent (init): consent should be an object.');
+
+    function test2 () {
+      cookieConsent.initConsent({
+        key1: 1
+      });
+    }
+
+    expect(test2).to.throw(TypeError, 'cookieConsent (init): consent values for key1 should be of type boolean.');
+
+    function test3 () {
+      return cookieConsent.initConsent({
+        key1: false
+      });
+    }
+
+    expect(test3()).to.be.true;
+  });
+
+  it('cookieConsent.getConsent', function () {
+    expect(cookieConsent.getConsent('key1')).to.be.false;
+    cookieConsent.setConsent('key1', true);
+    expect(cookieConsent.getConsent('key1')).to.be.true;
+  });
+
+  it('cookieConsent.setConsent', function () {
+    function test1 () {
+      cookieConsent.setConsent(1, 2);
+    }
+
+    expect(test1).to.throw(TypeError, 'cookieConsent (setConsent): consent key should be of type string.');
+
+    function test2 () {
+      cookieConsent.setConsent('key2', 1);
+    }
+
+    expect(test2).to.throw(TypeError, 'cookieConsent (setConsent): consent value should be of type boolean.');
+
+    function test3 () {
+      return cookieConsent.setConsent('key2', false);
+    }
+
+    expect(test3()).to.be.true;
+  });
+
+  it('cookieConsent.subscribe', function () {
+    function test1 () {
+      cookieConsent.subscribe(1, 2, null);
+    }
+
+    expect(test1).to.throw(TypeError, 'cookieConsent (subscribe): consent key should be of type string.');
+
+    function test2 () {
+      cookieConsent.subscribe('key1', 2, null);
+    }
+
+    expect(test2).to.throw(TypeError, 'cookieConsent (subscribe): event name should be of type string.');
+
+    function test3 () {
+      cookieConsent.subscribe('key1', 'delete', null);
+    }
+
+    var events = ['add', 'enable', 'disable'];
+    expect(test3).to.throw(TypeError, 'cookieConsent (subscribe): event name should be one of ' + events.join(', ') + '.');
+
+    function test4 () {
+      cookieConsent.subscribe('key1', 'enable', null);
+    }
+
+    expect(test4).to.throw(TypeError, 'cookieConsent (subscribe): callback should be a function.');
+
+    function test5 () {
+      return cookieConsent.subscribe('key1', 'enable', function () {});
+    }
+
+    expect(test5()).to.be.an('object');
+  });
+});
