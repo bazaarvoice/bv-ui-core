094e51339b6a975c3ba4d9007236b726e74e7f1d
Shubhakar-A <shubhakar.a@bazaarvoice.com>
2023-08-03 22:10:24 +0530

    [utility fix] fix issue when global is polluted

--- test/unit/global/index.spec.js
+++ test/unit/global/index.spec.js
@@ -15,4 +15,13 @@ describe('lib/global', function () {
     }
   });
 
+  it('should export window with default pointing to windows if __esModule is set', function () {
+    // If we have access to the window object, compare against it.
+    if (typeof window !== 'undefined') {
+      window.__esModule = true
+      var global = require('../../../lib/global')
+      expect(global).to.eql(window);
+    }
+  });
+
 });
